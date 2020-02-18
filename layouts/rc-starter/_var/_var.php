c020bfead5e0e763cff1759a082105bc502bf421
Gitaek kwon <break@kimsq.com>
2020-02-18 16:27:53 +0900

    Update _var.php

--- layouts/rc-starter/_var/_var.php
+++ layouts/rc-starter/_var/_var.php
@@ -3,6 +3,6 @@ $d['layout']['header_title'] = "마이홈";
 $d['layout']['header_file'] = "";
 $d['layout']['header_search'] = "true";
 $d['layout']['header_noti'] = "true";
-$d['layout']['main_type'] = "postFeed";
+$d['layout']['main_type'] = "postAllFeed";
 $d['layout']['kakao_key'] = "";
 ?>
