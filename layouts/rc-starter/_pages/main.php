f58c4e5b2974130f88d641d642bdf154d07a16b4
Gitaek kwon <break@kimsq.com>
2020-02-19 06:19:41 +0900

    Update main.php

--- layouts/rc-starter/_pages/main.php
+++ layouts/rc-starter/_pages/main.php
@@ -4,10 +4,16 @@ if ($my['uid'] && $d['layout']['main_dashboard']=='true') getLink('/dashboard','
 if ($d['layout']['main_type']=='postAllFeed') {
   getWidget('post/rc-default/post/all/feed-card',array('wrapper'=>'[data-role="postFeed"].widget','start'=>'#page-main','recnum'=>5));
 } else {
-
   $g['layoutPageVForSite'] = $g['path_var'].'site/'.$r.'/layout.'.$layout.'.main.php';   // 레이아웃 메인페이지 웨젯설정
   include is_file($g['layoutPageVForSite']) ? $g['layoutPageVForSite'] : $g['dir_layout'].'_var/_var.main.php';
   getWidgetList($d['layout']['main_widgets']);
-
 }
 ?>
+
+<?php if (!$d['layout']['main_widgets']): ?>
+<div class="alert alert-danger text-xs-center border-0 rounded-0" role="alert">
+  <a data-href="<?php echo $g['s'].'/?r='.$r.'&amp;layoutPage=settings&prelayout=rc-starter/blank' ?>" class="alert-link">
+    <u>메인 꾸미기가 필요합니다.</u>
+  </a>
+</div>
+<?php endif; ?>
