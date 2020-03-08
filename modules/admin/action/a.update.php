7f0e927061cdb85501f5c21e968d110429630cc6
Gitaek kwon <break@kimsq.com>
2020-03-08 09:32:23 +0900

    Update a.update.php

--- modules/admin/action/a.update.php
+++ modules/admin/action/a.update.php
@@ -4,14 +4,14 @@ if(!defined('__KIMS__')) exit;
 checkAdmin(0);
 
 $mbruid	= $my['uid'];
-$command1	= 'git reset --hard';
-$command2	= 'git pull origin master';
+$command_reset	= 'git reset --hard';
+$command_pull	= 'git pull origin master';
 $d_regis	= $date['totime'];
 $version = $current_version.'->'.$lastest_version;
 
-shell_exec($command1.'; echo $?');
-$output = shell_exec($command2.'; echo $?');
-$command	= $command1.'  '.$command2;
+$output_reset = shell_exec($command_reset.'; echo $?');
+$output_pull = shell_exec($command_pull.'; echo $?');
+$command	= $command_reset.' '.$command_pull;
 
 if ($g['mobile']&&$_SESSION['pcmode']!='Y') {
   $msg_type = 'default';
@@ -19,11 +19,11 @@ if ($g['mobile']&&$_SESSION['pcmode']!='Y') {
   $msg_type = 'success';
 }
 
-if(strpos($output, 'Already up-to-date.') !== false) {
+if(strpos($output_pull, 'Already up-to-date.') !== false) {
   $msg = '이미 최신버전 입니다.|'.$msg_type;
 } else {
-  getDbInsert($table['s_gitlog'],'mbruid,remote,command,version,output,d_regis',"'$mbruid','$remote','$command','$version','$output','$d_regis'");
-  $msg = '업데이트가 완료 되었습니다.|'.$msg_type;
+  getDbInsert($table['s_gitlog'],'mbruid,remote,command,version,output,d_regis',"'$mbruid','$remote','$command','$version','$output_pull','$d_regis'");
+  $msg = '업데이트가 완료 되었습니다. 브라우저를 재시작해주세요.|'.$msg_type;
 }
 $_SESSION['current_version'] = $lastest_version;
 setrawcookie('system_update_result', rawurlencode($msg));  // 알림처리를 위한 로그인 상태 cookie 저장
