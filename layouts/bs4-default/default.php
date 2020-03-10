0b2ae8277ba39cf93c88a38ba45e0308427bc643
Gitaek kwon <break@kimsq.com>
2020-03-10 12:48:37 +0900

    Update default.php

--- layouts/bs4-default/default.php
+++ layouts/bs4-default/default.php
@@ -14,12 +14,12 @@
 	<?php if ($c && $d['layout']['default_titlebar']!='false') include $g['dir_layout'].'/_includes/subtitlebar.'.$d['layout']['default_titlebar'].'.php' ?>
 
 	<main role="main" class="<?php echo $d['layout']['default_bgcolor'] ?>">
-		<div class="<?php echo $d['layout']['default_container'] ?> px-0 py-4">
+		<div class="<?php echo $d['layout']['default_container'] ?> py-4">
 
 			<?php if ($c && $d['layout']['default_menutitle']!='false')
 				include $g['dir_layout'].'/_includes/submenutitle.'.$d['layout']['default_menutitle'].'.php' ?>
 
-				<article class="rb-article">
+				<article>
 					<?php include __KIMS_CONTENT__ ?>
 				</article>
 
