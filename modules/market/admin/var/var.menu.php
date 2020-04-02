aeda13049c78d8625cd5f8d832588dce6fb577a1
Gitaek kwon <break@kimsq.com>
2020-04-03 06:02:33 +0900

    Update var.menu.php

--- modules/market/admin/var/var.menu.php
+++ modules/market/admin/var/var.menu.php
@@ -2,7 +2,7 @@
 $d['amenu']['main']		= '전체상품';
 $d['amenu']['recomm']= '추천상품';
 $d['amenu']['brand']	= '브랜드샵';
-$d['amenu']['buyer']	= '구입목록';
 $d['amenu']['update']	= '설치내역/업데이트';
+$d['amenu']['buyer']	= '구매내역';
 $d['amenu']['config']	= '설정';
 ?>
