8ed7402b5fe851f9ede05f9f379f1644793c9e09
Gitaek kwon <break@kimsq.com>
2020-04-07 06:24:35 +0900

    Update main.php

--- layouts/rc-starter/_pages/main.php
+++ layouts/rc-starter/_pages/main.php
@@ -2,8 +2,8 @@
 if ($my['uid'] && $d['layout']['main_dashboard']=='true') getLink('/dashboard','','','');
 
 if ($d['layout']['main_type']=='postAllFeed') {
-  getWidget('site/rc-default/cover-default',array());
-  getWidget('post/rc-default/post/all/feed-card',array('wrapper'=>'[data-role="postFeed"].widget','start'=>'#page-main','recnum'=>5));
+  getWidget('rc-default/site/cover-default',array());
+  getWidget('rc-default/post/post/all/feed-card',array('wrapper'=>'[data-role="postFeed"].widget','start'=>'#page-main','recnum'=>5));
 } else {
   $g['layoutPageVForSite'] = $g['path_var'].'site/'.$r.'/layout.'.$layout.'.main.php';   // 레이아웃 메인페이지 웨젯설정
   include is_file($g['layoutPageVForSite']) ? $g['layoutPageVForSite'] : $g['dir_layout'].'_var/_var.main.php';
