f8fa5a725571539d77ee83e3dd8d569410827626
Gitaek kwon <break@kimsq.com>
2020-04-08 14:13:36 +0900

    시스템 본문 허용태그 추가

--- modules/admin/var/var.system.php
+++ modules/admin/var/var.system.php
@@ -26,7 +26,7 @@ $d['admin']['ssl_menu'] = "";
 $d['admin']['ssl_page'] = "";
 $d['admin']['ssl_bbs'] = "";
 $d['admin']['ssl_module'] = "";
-$d['admin']['secu_tags'] = "img[src|alt|title|width|height|style],figure[class], figcaption,oembed[url],video[src|type|width|height|poster|preload|controls], source[src|type],a[href|target],iframe[width|height|src|frameborder|allowfullscreen],strong, b, i, u, em, br, font,h1[style], h2[style], h3[style], h4[style], h5[style], h6[style],p[style], div[style], center, address[style],span[style], pre[style],ul, ol, li,table[width|height|border|style], th[width|height|border|style],tr[width|height|border|style], td[width|height|border|style],hr,blockquote";
+$d['admin']['secu_tags'] = "img[src|alt|title|width|height|style],figure[class], figcaption,oembed[url],video[src|type|width|height|poster|preload|controls], source[src|type],a[href|target],iframe[width|height|src|frameborder|allowfullscreen],strong, b, i, u, s, mark, code, em, br, font,h1[style], h2[style], h3[style], h4[style], h5[style], h6[style],p[style], div[style], center, address[style],span[style], pre[style],ul, ol, li,table[width|height|border|style], th[width|height|border|style],tr[width|height|border|style], td[width|height|border|style],hr,blockquote";
 $d['admin']['secu_flash'] = "";
 $d['admin']['secu_domain'] = "youtube.com,vimeo.com,";
 $d['admin']['secu_param'] = ";a=,&a=,?a=,m=admin,system=,";
