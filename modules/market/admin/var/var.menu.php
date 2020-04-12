ac3fafbc062e59f03315b97c2b1c103f1d19d85d
Gitaek kwon <break@kimsq.com>
2020-04-12 11:37:27 +0900

    마켓 개선

--- modules/market/admin/var/var.menu.php
+++ modules/market/admin/var/var.menu.php
@@ -1,6 +1,6 @@
 <?php
-$d['amenu']['main']		= '익스텐션';
-$d['amenu']['package'] = '패키지';
+$d['amenu']['main'] = '패키지';
+$d['amenu']['extension'] = '익스텐션';
 $d['amenu']['recomm']= '추천상품';
 $d['amenu']['brand']	= '브랜드';
 $d['amenu']['update']	= '설치내역';
