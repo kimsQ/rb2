55e841787cb93ea1d46626578b7a34f7e4c105ab
Gitaek kwon <break@kimsq.com>
2020-06-01 14:27:01 +0900

    설치완료 페이지 QR-code 출력

--- modules/site/pages/guide.install.php
+++ modules/site/pages/guide.install.php
@@ -1,4 +1,3 @@
-
 <div class="text-center" style="margin-top: -100px">
 	<h1>
 		<i class="kf kf-bi-06 fa-lg d-block mb-4" title="KimsQ is Kind" data-tooltip="tooltip"></i>
@@ -8,11 +7,14 @@
 		<?php echo sprintf('%s님, 이 페이지를 보고 계시면 킴스큐가 정상적으로 설치된 것입니다.',$my['name'])?><br>
 		이제 킴스큐를 시작할 준비가 되셨습니다.
 	</p>
+
+	<div id="qrcode"></div>
+
 	<a href="<?php  echo RW(0) ?>" class="btn btn-primary btn-lg animated bounce delay-3">
 		시작하기
 	</a>
 </div>
-
+<script src="//cdn.jsdelivr.net/npm/davidshimjs-qrcodejs@0.0.2/qrcode.min.js"></script>
 <script>
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -20,4 +22,13 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 ga('create', 'UA-55876126-3', 'auto');
 ga('send', 'pageview');
+
+var qrcode = new QRCode(document.getElementById("qrcode"), {
+	text: '<?php echo $g['url_root'].$g['s'] ?>',
+	width: 150,
+	height: 150,
+	colorDark : "#333",
+	colorLight : "#f8f9fa",
+	correctLevel : QRCode.CorrectLevel.H
+});
 </script>
