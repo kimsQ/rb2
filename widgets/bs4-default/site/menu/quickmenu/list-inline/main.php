1fdcda70a124ac81551ac8599c200ab27088a0b7
Gitaek kwon <break@kimsq.com>
2020-06-17 06:46:19 +0900

    기능개선

--- widgets/bs4-default/site/menu/quickmenu/list-inline/main.php
+++ widgets/bs4-default/site/menu/quickmenu/list-inline/main.php
@@ -1,6 +1,8 @@
 <?php
-  $_MENUQ1=getDbData($table['s_menu'],'site='.$s." and id='".$wdgvar['smenu']."'",'uid,name');
-  $_MENUQ2=getDbSelect($table['s_menu'],'site='.$s.' and parent='.$_MENUQ1['uid'].' and hidden=0 order by gid asc','*');
+$wdgvar_smenu_arr = explode('/' , $wdgvar['smenu']);
+$wdgvar_smenu_arr_end =  array_pop($wdgvar_smenu_arr);
+$_MENUQ1=getDbData($table['s_menu'],'site='.$s." and id='".$wdgvar_smenu_arr_end."'",'uid,name');
+$_MENUQ2=getDbSelect($table['s_menu'],'site='.$s.' and parent='.$_MENUQ1['uid'].' and hidden=0 order by gid asc','*');
 ?>
 
 <?php while($_M2=db_fetch_array($_MENUQ2)):?>
