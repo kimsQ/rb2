07b99461ed396c4f5c3aeab200d7ed7a482590df
Gitaek kwon <break@kimsq.com>
2020-07-03 09:28:08 +0900

    사이트 설정 경로변수 변경

--- layouts/bs4-starter/_includes/_import.control.php
+++ layouts/bs4-starter/_includes/_import.control.php
@@ -1,16 +1,16 @@
 <?php
 //사이트별 레이아웃 설정 변수
 $layout = dirname($_HS['layout']);
-$g['layoutVarForSite'] = $g['path_var'].'site/'.$r.'/layout.'.$layout.'.var.php';
+$g['layoutVarForSite'] = $g['dir_var_site'].'layout.'.$layout.'.var.php';
 include is_file($g['layoutVarForSite']) ? $g['layoutVarForSite'] : $g['dir_layout'].'_var/_var.php';
 
 //사이트 부가정보 변수
-$g['siteinfo'] = $g['path_var'].'site/'.$r.'/siteinfo.php';
+$g['siteinfo'] = $g['dir_var_site'].'siteinfo.php';
 if (is_file($g['siteinfo'])) include $g['siteinfo'];
 
 //사이트별 웹앱 매니페스트
-$g['manifestForSite'] = $g['path_var'].'site/'.$r.'/manifest.json';
-$g['url_manifest'] = $g['s'].'/_var/site/'.$r.'/manifest.json';
+$g['manifestForSite'] = $g['dir_var_site'].'manifest.json';
+$g['url_manifest'] = $g['url_var_site'].'/manifest.json';
 $manifestForSite = file_exists($g['manifestForSite']) ? $g['url_manifest'] : $g['path_module'].'site/var/manifest.json';
 
 if ($layoutPage) {
--- layouts/rc-starter/_includes/_import.control.php
+++ layouts/rc-starter/_includes/_import.control.php
@@ -1,21 +1,21 @@
 <?php
 //사이트별 레이아웃 설정 변수
 $layout = dirname($_HS['m_layout']);
-$g['layoutVarForSite'] = $g['path_var'].'site/'.$r.'/layout.'.$layout.'.var.php';
+$g['layoutVarForSite'] = $g['dir_var_site'].'layout.'.$layout.'.var.php';
 include is_file($g['layoutVarForSite']) ? $g['layoutVarForSite'] : $g['dir_layout'].'_var/_var.php';
 
 //사이트 부가정보 변수
-$g['siteinfo'] = $g['path_var'].'site/'.$r.'/siteinfo.php';
+$g['siteinfo'] = $g['dir_var_site'].'siteinfo.php';
 if (is_file($g['siteinfo'])) include $g['siteinfo'];
 
 //사이트별 웹앱 매니페스트
-$g['manifestForSite'] = $g['path_var'].'site/'.$r.'/manifest.json';
-$g['url_manifest'] = $g['s'].'/_var/site/'.$r.'/manifest.json';
+$g['manifestForSite'] = $g['dir_var_site'].'manifest.json';
+$g['url_manifest'] = $g['url_var_site'].'/manifest.json';
 $manifestForSite = file_exists($g['manifestForSite']) ? $g['url_manifest'] : $g['path_module'].'site/var/manifest.json';
 
 //아이폰 전용 apple-touch-icon
-$g['touchIconForSite'] = $g['path_var'].'site/'.$r.'/homescreen.png';
-$g['url_touchIcon'] = $g['s'].'/_var/site/'.$r.'/homescreen-180x180.png';
+$g['touchIconForSite'] = $g['url_var_site'].'/homescreen.png';
+$g['url_touchIcon'] = $g['url_var_site'].'/homescreen-180x180.png';
 $touchIconForSite = file_exists($g['touchIconForSite']) ? $g['url_touchIcon'] : $g['img_core'].'/touch/homescreen-180x180.png';
 
 if ($layoutPage) {
